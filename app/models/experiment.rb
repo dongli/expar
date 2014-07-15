be8c03946ef4a0fe29d2868346dba0434eb9b32e
Li Dong <dongli@lasg.iap.ac.cn>
2014-07-15 18:01:50 +0800

    Fix two bugs
    The experiment title should only be checked for uniqueness when creation.
    
    The partial _form.html.erb for experiments view has wrong form_for
    arguments, so fix them. And also add judgement about the new or old of
    the experiment to automatically set the current user name and mail.

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,7 +1,8 @@
 class Experiment < ActiveRecord::Base
   has_many :figures, dependent: :destroy, inverse_of: :experiment
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: true, :on => :create
   validates :date, presence: true
   validates :author, presence: true
   validates :mail, presence: true

ff74a8140f7fa9569dedf49425d1be8f0d9eea2b
Li Dong <dongli@lasg.iap.ac.cn>
2014-07-15 16:14:59 +0800

    Fix a bug
    The first_name and last_name for Devise User model are not permitted, so
    permit them.
    
    Polish the front-end.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713082314) do
+ActiveRecord::Schema.define(version: 20140714153511) do
 
   create_table "experiments", force: true do |t|
     t.string   "title"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20140713082314) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

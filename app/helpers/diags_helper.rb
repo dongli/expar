2dbd7290dc319891fb7d1e9654cde406c4409e75
Li Dong <dongli@lasg.iap.ac.cn>
2014-07-23 11:32:42 +0800

    Add user gravatar support
    Also add breadcrumbs for better navigation among pages.

--- app/helpers/diags_helper.rb
+++ app/helpers/diags_helper.rb
@@ -1,2 +1,6 @@
 module DiagsHelper
+  def get_diag_experiment(id)
+    diag = Diag.find(id)
+    experiment = Experiment.find(diag.experiment_id)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
   def get_admin_email
     return User.find(1).email
   end
+
+  def get_gravatar(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://www.gravatar.com/avatar.php?"
+    gravatar_url << "gravatar_id=#{gravatar_id}"
+    gravatar_url << "&size=160px"
+    image_tag(gravatar_url, alt: get_user_name(user), class: "gravatar")
+  end
 end
